d0eaeceddf0811d32a1b15f96c8bfa05ad2cafd9
hphuc0324 <sippermana3@gmail.com>
2024-09-09 11:26:43 +0700

    bind data filename

--- src/data/actionsDB.js
+++ src/data/actionsDB.js
@@ -3,20 +3,18 @@ const path = require('path');
 
 const mongoose = require('mongoose');
 const { host, name } = require('../configs/config.mongodb');
+const dataField = require('./dataField');
 
 const connectString = `${host}/${name}?retryWrites=true&w=majority`;
 
-// mongoose
-//     .connect(connectString)
-//     .then((_) => {
-//         console.log('Connected to MongoDB');
-//     })
-//     .catch((error) => {
-//         console.error('Failed to connect to MongoDB', error);
-//     });
-
-//READ FILES
-// const icons = JSON.parse(fs.readFileSync('./icon.json'), 'utf-8');
+mongoose
+    .connect(connectString)
+    .then((_) => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+        console.error('Failed to connect to MongoDB', error);
+    });
 
 //LOAD DATA
 const loadData = async (file) => {
@@ -41,23 +39,65 @@ const importData = async (data, model) => {
     } catch (error) {
         console.log(`Error importing data`, error);
     }
+};
 
-    process.exit();
+//UPDATE DATA
+const updateData = async (data, model, field) => {
+    try {
+        for (const item of data) {
+            await model.updateOne(
+                {
+                    [field]: item[field],
+                },
+                {
+                    $set: item,
+                },
+                { upsert: true },
+            );
+        }
+
+        console.log('Update data successfully');
+    } catch (error) {
+        console.log(`Error updating data`, error);
+    }
+};
+
+//DELETE DATA
+const deleteData = async (model) => {
+    try {
+        await model.deleteMany();
+
+        console.log('Delete data successfully');
+    } catch (error) {
+        console.log(`Error deleting data`, error);
+    }
 };
 
 //RUN
 const run = async () => {
     const mode = process.argv[2];
     const file = process.argv[3];
+    const dataModel = dataField[file].data_model;
 
     console.log('Mode::', mode);
 
     if (mode === '--delete') {
+        await deleteData(dataModel);
     } else {
         const data = await loadData(file);
 
-        console.log('Data::', data);
+        if (mode === '--import') {
+            await importData(data, dataModel);
+        } else if (mode === '--update') {
+            const field = dataField[file].main_field;
+
+            await updateData(data, dataModel, field);
+        } else {
+            console.log('Invalid mode');
+        }
     }
+
+    process.exit();
 };
 
 run();
---
+++ src/data/dataField.js
@@ -0,0 +1,10 @@
+const iconModel = require('../models/icon.model');
+
+const dataField = {
+    'icon.json': {
+        main_field: 'icon_label',
+        data_model: iconModel,
+    },
+};
+
+module.exports = dataField;
