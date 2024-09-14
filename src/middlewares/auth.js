4828eb62035c6e36f9f7a5da51ee0f79928bc5b9
hphuc0324 <sippermana3@gmail.com>
2024-09-14 18:29:51 +0700

    firbase-admin config

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,18 +1,18 @@
-const { AuthFailureError } = require('../constants/error.respone');
-const admin = require('firebase-admin');
+const { AuthFailureError, ForbiddenRequest } = require('../constants/error.respone');
+const admin = require('../configs/config.firebase');
 
 const authentication = async (req, res, next) => {
     const token = req.headers.authorization?.split('Bearer ')[1];
 
-    // if (!token) {
-    //     console.log('Authentication failed');
-    //     throw new AuthFailureError();
-    // }
+    if (!token) {
+        console.log('Authentication failed');
+        throw new AuthFailureError();
+    }
 
     const decodedToken = await admin.auth().verifyIdToken(token);
 
     if (!decodedToken) {
-        throw new AuthFailureError();
+        throw new ForbiddenRequest();
     }
 
     req.uid = decodedToken.uid;
