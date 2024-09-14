ff6d41da51c3a84bdc7d6531ddab50c8ab3f0e1a
hphuc0324 <sippermana3@gmail.com>
2024-09-14 13:08:45 +0700

    install firebase-admin

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,12 +1,22 @@
 const { AuthFailureError } = require('../constants/error.respone');
+const admin = require('firebase-admin');
 
 const authentication = async (req, res, next) => {
     const token = req.headers.authorization?.split('Bearer ')[1];
 
-    if (!token) {
-        console.log('Authentication failed');
+    // if (!token) {
+    //     console.log('Authentication failed');
+    //     throw new AuthFailureError();
+    // }
+
+    const decodedToken = await admin.auth().verifyIdToken(token);
+
+    if (!decodedToken) {
         throw new AuthFailureError();
     }
+
+    req.uid = decodedToken.uid;
+    next();
 };
 
 module.exports = { authentication };
